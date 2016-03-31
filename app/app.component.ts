2e3de823770cc1880daa18e9d09570fb874debe5
Dmitriy Schekhovtsov <valorkin@gmail.com>
2016-04-01 00:28:15 +0300

    feat(package): udpated to beta.12 and ng2-bootstrap 1.0.9

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,33 @@
 import {Component} from 'angular2/core';
-import {Alert} from 'ng2-bootstrap/ng2-bootstrap';
+import {Alert, DATEPICKER_DIRECTIVES} from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
   selector: 'my-app',
-  directives: [Alert],
+  directives: [Alert, DATEPICKER_DIRECTIVES],
   template: `
     <alert type="info">ng2-bootstrap hello world!</alert>
+      <pre>Selected date is: <em *ngIf="dt">{{ getDate() | date:'fullDate'}}</em></pre>
+      <h4>Inline</h4>
+      <div style="display:inline-block; min-height:290px;">
+        <datepicker [(ngModel)]="dt" [minDate]="minDate" [showWeeks]="true"></datepicker>
+      </div>
   `,
 })
 export class AppComponent {
+  public dt:Date = new Date();
+  private minDate:Date = null;
+  private events:Array<any>;
+  private tomorrow:Date;
+  private afterTomorrow:Date;
+  private formats:Array<string> = ['DD-MM-YYYY', 'YYYY/MM/DD', 'DD.MM.YYYY', 'shortDate'];
+  private format = this.formats[0];
+  private dateOptions:any = {
+    formatYear: 'YY',
+    startingDay: 1
+  };
+  private opened:boolean = false;
+
+  public getDate():number {
+    return this.dt && this.dt.getTime() || new Date().getTime();
+  }
 }
